8384f473cf79224d5c86ef0bbc70f69acb6da748
David Hail <dhail@ymail.com>
2016-10-10 14:11:27 -0700

    changed class variables to instance variables

--- hangman.rb
+++ hangman.rb
@@ -3,16 +3,14 @@ require 'sinatra/reloader'
 require 'open-uri'
 
 class Hangman
-
-	@@array_word = []
-	@@array_good_guesses = []
-	@@array_bad_guesses = []
 	
 	def initialize
-		
+		@array_word = []
+		@array_good_guesses = []
+		@array_bad_guesses = []
 	end
 
-	def self.get_word(letters)
+	def get_word(letters)
 
 		array_matching_words = [] #To hold all words in dictionary with number of letters seleted by player
 		url = "http://s3.amazonaws.com/viking_education/web_development/web_app_eng/enable.txt"
@@ -30,49 +28,51 @@ class Hangman
 		random_word_number = rand(array_matching_words.length - 1) #Position of the word in array
 		random_word = array_matching_words[random_word_number].chomp
 
-		@@array_word = random_word.split(//)
+		@array_word = random_word.split(//)
 
-		@@array_good_guesses = []
+		@array_good_guesses = []
 
-		(@@array_word.length).times do |num|
-			@@array_good_guesses[num] = '&nbsp;'
+		(@array_word.length).times do |num|
+			@array_good_guesses[num] = '&nbsp;'
 		end
 
-		return @@array_bad_guesses, @@array_good_guesses, @@array_word
+		return @array_bad_guesses, @array_good_guesses, @array_word
 	end
 
-	def self.check_guess(guess_letter)
+	def check_guess(guess_letter)
 
 		guess_letter = guess_letter.downcase
 
-		@@array_word.each_with_index do |word_letter, letter_position|
+		@array_word.each_with_index do |word_letter, letter_position|
 			
 			if guess_letter == word_letter
-				@@array_good_guesses[letter_position] = word_letter
+				@array_good_guesses[letter_position] = word_letter
 			end
 		end
 
-		unless @@array_good_guesses.include?(guess_letter)
+		unless @array_good_guesses.include?(guess_letter)
 			
-			unless @@array_bad_guesses.include?(guess_letter)
-				@@array_bad_guesses << guess_letter
+			unless @array_bad_guesses.include?(guess_letter)
+				@array_bad_guesses << guess_letter
 			end
 
-			return @@array_bad_guesses, @@array_good_guesses, @@array_word
+			return @array_bad_guesses, @array_good_guesses, @array_word
 		end
-		return @@array_bad_guesses, @@array_good_guesses, @@array_word
+		return @array_bad_guesses, @array_good_guesses, @array_word
 	end
 
-	def self.new_game
+	def new_game
 
-		@@array_word = []
-		@@array_good_guesses = nil
-		@@array_bad_guesses = []
+		@array_word = []
+		@array_good_guesses = nil
+		@array_bad_guesses = []
 		
-		return @@array_bad_guesses, @@array_good_guesses, @@array_word
+		return @array_bad_guesses, @array_good_guesses, @array_word
 
 	end
 end
+
+my_game = Hangman.new
 
 get '/' do
 	
@@ -82,13 +82,13 @@ get '/' do
 		else
 			make = params['make'].to_i
 		end
-		bad_guesses, good_guesses, word = Hangman.get_word(make)
+		bad_guesses, good_guesses, word = my_game.get_word(make)
 	elsif params['guess'] != nil
 		guess = params['guess'].to_s
-		bad_guesses, good_guesses, word = Hangman.check_guess(guess)
+		bad_guesses, good_guesses, word = my_game.check_guess(guess)
 	elsif params['game'] != nil
 		game = params['game'].to_s
-		bad_guesses, good_guesses, word = Hangman.new_game
+		bad_guesses, good_guesses, word = my_game.new_game
 	end
   erb :index, :locals => {:bad_guesses => bad_guesses, :good_guesses => good_guesses, :word => word}
 end
